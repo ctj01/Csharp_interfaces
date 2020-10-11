0191968dcefc5c9d7a24f948defc06c7bf4f2c80
Cristian <Cristian@DESKTOP-3RO8NFF>
2020-10-11 07:49:12 -0500

    ICloneable

---
+++ interfaces/IClonable/Auto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IClonable
+{
+    public class Auto : Automovil, ICloneable
+    {
+        private double costo;
+        string modelo;
+        private double tenencia;
+        public Auto(double Pcosto, string Pmodelo)
+        {
+            Costo = Pcosto;
+            Modelo = Pmodelo;
+        }
+
+        public double Costo { get => costo; set => costo = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public double Tenencia { get => tenencia; set => tenencia = value; }
+
+        public void CalculaImpuesto(double imp)
+        {
+            tenencia = 3000.00 + costo * imp;
+        }
+
+        public object Clone()
+        {
+            Auto nuevo = new Auto(costo, modelo);
+            nuevo.Tenencia = tenencia;
+            return nuevo;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("el modelo del auto es {0} y tiene un precio de {1} y un impuesto de {2} ", modelo, costo, tenencia);
+            Console.WriteLine("----------------------");
+        }
+    }
+}
---
+++ interfaces/IClonable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IClonable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///sin clonacion
+            Auto auto1 = new Auto(50000.00, "MAZDA");
+            Auto auto2 = auto1;
+            auto1.CalculaImpuesto(0.10);
+            auto2.CalculaImpuesto(0.10);
+            auto1.MostrarInformacion();
+            auto2.MostrarInformacion();
+            auto1.Costo = 60000.00;
+            auto1.MostrarInformacion();
+            auto2.MostrarInformacion();
+
+            //clonancion
+            Auto auto3 = (Auto)auto1.Clone();
+            auto1.MostrarInformacion();
+            auto3.MostrarInformacion();
+            Console.WriteLine("================================");
+            auto1.Costo = 900000.00;
+            auto1.MostrarInformacion();
+            auto3.MostrarInformacion();
+
+        }
+    }
+}
