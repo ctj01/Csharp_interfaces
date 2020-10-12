31f01cc7fa135e6e87ec95b45cc57cc16196b1ec
Cristian <Cristian@DESKTOP-3RO8NFF>
2020-10-12 06:32:00 -0500

    IComparable

---
+++ interfaces/Icompare/CRectangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icompare
+{
+    class CRectangulo:IComparable
+    {
+        private int alto;
+        private int ancho;
+        private int area;
+        public CRectangulo(int Palto, int Pancho)
+        {
+            alto = Palto;
+            ancho = Pancho;
+            calculaArea();
+
+        }
+        void calculaArea() 
+        {
+            area = ancho * alto;
+        }
+        public override string ToString()
+        {
+            return string.Format("el alto del rectangulo es {0} y el ancho es {1} y su area es {2}", alto , ancho, area);
+        }
+
+        public int CompareTo(object obj)
+        {
+            CRectangulo nuevo = (CRectangulo)obj;
+            if (area > nuevo.area)
+            {
+                return (1);
+            }
+            else if (area < nuevo.area)
+            {
+                return (-1);
+            }
+            return (0);
+        }
+    }
+}
---
+++ interfaces/Icompare/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CRectangulo[] rects = new CRectangulo[5];
+            rects[0] = new CRectangulo(5, 10);
+            rects[1] = new CRectangulo(15, 20);
+            rects[2] = new CRectangulo(15, 9);
+            rects[3] = new CRectangulo(21, 3);
+            rects[4] = new CRectangulo(15, 12);
+            Array.Sort(rects);
+
+            foreach (CRectangulo item in rects)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
